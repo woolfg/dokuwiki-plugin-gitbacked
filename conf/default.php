2dbc1a5564516b801dbda239b68152edb5be0303
Wolfgang Gassler <wolfgang@gassler.org>
2012-11-13 10:19:44 +0100

    default commit messages changed

--- conf/default.php
+++ conf/default.php
@@ -8,9 +8,9 @@
 $conf['pushAfterCommit'] = 0;
 $conf['periodicPull'] = 0;
 $conf['periodicMinutes'] = 60;
-$conf['commitPageMsg']	= '%page% changed with %summary% by %user%';
-$conf['commitPageMsgDel']	= '%page% deleted %summary% by %user%';
-$conf['commitMediaMsg']	= '%media% uploaded by %user%';
-$conf['commitMediaMsgDel']	= '%media% deleted by %user%';
+$conf['commitPageMsg']	= 'Wiki page %page% changed with summary [%summary%] by %user%';
+$conf['commitPageMsgDel']	= 'Wiki page %page% deleted with reason [%summary%] by %user%';
+$conf['commitMediaMsg']	= 'Wiki media %media% uploaded by %user%';
+$conf['commitMediaMsgDel']	= 'Wiki media %media% deleted by %user%';
 $conf['repoPath']	= $GLOBALS['conf']['savedir'];
 $conf['addParams'] = '';
