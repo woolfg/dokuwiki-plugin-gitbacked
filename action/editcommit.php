af32950075192d9f5102edaac8784588c5e3b4c3
Robin H. Johnson <robbat2@gentoo.org>
2013-11-25 14:32:33 -0800

    editcommit: message improvements
    Allow more flexibility in commit message tools (emails, full paths).
    
    Signed-off-by: Robin H. Johnson <robbat2@gentoo.org>

--- action/editcommit.php
+++ action/editcommit.php
@@ -121,8 +121,8 @@ class action_plugin_gitbacked_editcommit extends DokuWiki_Action_Plugin {
         $mediaName = $event->data[2];
 
         $message = str_replace(
-            array('%media%','%user%'),
-            array($mediaName,$this->getAuthor()),
+            array('%media%','%user%','%mail%'),
+            array($mediaName,$this->getAuthor(),$this->getAuthorMail()),
             $this->getConf('commitMediaMsg')
         );
 
@@ -140,9 +140,12 @@ class action_plugin_gitbacked_editcommit extends DokuWiki_Action_Plugin {
          */
         if (!$rev) {
 
-            $pagePath = $event->data[0][0];
+            $pageFsPath = $event->data[0][0];
+            $pageNsPath = $event->data[1];
             $pageName = $event->data[2];
             $pageContent = $event->data[0][1];
+
+            $fullPageName = str_replace(':', '/', $pageNsPath).'/'.$pageName;
 
             // get the summary directly from the form input
             // as the metadata hasn't updated yet
@@ -156,7 +159,7 @@ class action_plugin_gitbacked_editcommit extends DokuWiki_Action_Plugin {
                 // bad hack as DokuWiki deletes the file after this event
                 // thus, let's delete the file by ourselves, so git can recognize the deletion
                 // DokuWiki uses @unlink as well, so no error should be thrown if we delete it twice
-                @unlink($pagePath);
+                @unlink($pageFsPath);
 
             } else {
                 //get the commit text for edits
@@ -164,12 +167,13 @@ class action_plugin_gitbacked_editcommit extends DokuWiki_Action_Plugin {
             }
 
             $message = str_replace(
-                array('%page%','%summary%','%user%'),
-                array($pageName,$editSummary,$this->getAuthor()),
+                array('%fullpage%','%pagens%','%page%','%summary%','%user%','%mail%'),
+                array($fullPageName,$pageNsPath,$pageName,$editSummary,$this->getAuthor(),$this->getAuthorMail()),
+                //array($pageName,$editSummary,$this->getAuthor(),$this->getAuthorMail()),
                 $msgTemplate
             );
 
-            $this->commitFile($pagePath,$message);
+            $this->commitFile($pageFsPath,$message);
 
         }
 
