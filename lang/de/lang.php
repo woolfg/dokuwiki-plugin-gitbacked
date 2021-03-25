00aa0913d094e5025aac5d2a8dca95f0c1da2eb9
Markus Hoffrogge <mhoffrogge@gmail.com>
2021-03-25 22:37:59 +0100

    Extension to send error messages to a configurable eMail address
    - German translations added
    
    Signed-off-by: Markus Hoffrogge <mhoffrogge@gmail.com>

---
+++ lang/de/lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_create_new_error_subject'] = 'gitbacked plugin: FEHLER beim Anlegen eines neuen Git Repositories';
+$lang['mail_repo_path_error_subject'] = 'gitbacked plugin: Ein FEHLER mit dem konfigurierten Pfad zum Git Repository ist aufgetreten';
+$lang['mail_command_error_subject'] = 'gitbacked plugin: Ein Git Befehl ist FEHLGESCHLAGEN';
+$lang['mail_command_success_subject'] = 'gitbacked plugin: Ein Git Befehl wurde ausgeführt';
---
+++ lang/de/settings.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * German strings for the gitbacked plugin
+ *
+ * @author Markus Hoffrogge <mhoffrogge@gmail.com>
+ */
+
+$lang['pushAfterCommit'] = 'Push des aktiven Branch zum remote origin nach jedem commit';
+$lang['periodicPull'] = 'Pull des remote git Repositories alle "periodicMinutes", getriggert von einem http Page Request';
+$lang['periodicMinutes'] = 'Zeitraum (in Minuten) zwischen den periodischen pull requests';
+$lang['commitPageMsg']	= 'Commit Kommentar für Seitenänderungen (%user%,%summary%,%page% werden durch die tatsächlichen Werte ersetzt)';
+$lang['commitPageMsgDel']	= 'Commit Kommentar für gelöschte Seiten (%user%,%summary%,%page% werden durch die tatsächlichen Werte ersetzt)';
+$lang['commitMediaMsg']	= 'Commit Kommentar for media Dateien (%user%,%media% werden durch die tatsächlichen Werte ersetzt)';
+$lang['commitMediaMsgDel']	= 'Commit Kommentar für gelöschte media Dateien (%user%,%media% werden durch die tatsächlichen Werte ersetzt)';
+$lang['repoPath']	= 'Pfad des git repo (z.B. das <code>savedir</code> '.$GLOBALS['conf']['savedir'].')';
+$lang['repoWorkDir']	= 'Pfad des git working tree. Dieser muss die "pages" and "media" Verzeichnisse enthalten (z.B. das <code>savedir</code> '.$GLOBALS['conf']['savedir'].')';
+$lang['gitPath'] = 'Pfad zum git binary (Wenn leer, dann wird der Standard "/usr/bin/git" verwendet)';
+$lang['addParams'] = 'Zusätzliche git Parameter (diese werden dem git Kommando zugefügt) (%user% und %mail% werden durch die tatsächlichen Werte ersetzt)';
+$lang['ignorePaths'] = 'Pfade/Dateien die ignoriert werden und nicht von git archiviert werden sollen (durch Kommata getrennt)';
+$lang['emailAddressOnError'] = 'Wenn definiert, dann wird bei einem Fehler eine eMail an diese Adresse gesendet mit detaillierter Kontext Information und der git Fehlermeldung, anstatt den aktuellen Endanwender mit einer Exception zu verunsichern';
+$lang['notifyByMailOnSuccess'] = 'Wenn <code>emailAddressOnError</code> definiert ist, dann wird bei jedem Commit eine eMail gesendet. Diese Einstellung sollte nur zum Testen der eMail Benachrichtigung aktiviert werden';
