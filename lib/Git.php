cb48feea37ef0afec01ca238ba4b1cf79b1e47f2
mhoffrog <>
2021-11-28 18:12:52 +0100

    Extension to maintain pages and media in separate repos as well as backing conf and php code in dedicated repos
    - Git.php:
      - fixed code editing copy issue
    
    Signed-off-by: Markus Hoffrogge <mhoffrogge@gmail.com>

--- lib/Git.php
+++ lib/Git.php
@@ -138,7 +138,6 @@ class GitRepo {
 	protected $repo_path = null;
 	protected $bare = false;
 	protected $envopts = array();
-	protected ?\action_plugin_gitbacked_editcommit $plugin = null;
 	// @bugfix for PHP <= 7.3 compatibility: class property type declarations are supported by PHP >= 7.4 only
 	// protected ?\action_plugin_gitbacked_editcommit $plugin = null;
 	protected $plugin = null;
